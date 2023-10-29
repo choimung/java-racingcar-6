a26c4f012b2a4acf7e18c4a29247e096d3b13ed0
choimung <103022503+choimung@users.noreply.github.com>
2023-10-29 11:07:57 +0900

    style: Position클래스 코드컨벤션 적용 Position클래스 코드컨벤션 적용

--- src/main/java/racingcar/Car/Position.java
+++ src/main/java/racingcar/Car/Position.java
@@ -3,16 +3,15 @@ package racingcar.Car;
 public class Position {
     private int position;
 
-
     public Position(int position) {
         this.position = position;
     }
 
-    public int getPosition(){
+    public int getPosition() {
         return this.position;
     }
 
-    public void addPosition(){
+    public void addPosition() {
         position++;
     }
 }
