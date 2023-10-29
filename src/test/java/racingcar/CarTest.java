e81c30b8f85808f43feea7e228e63193c3441ae2
choimung <103022503+choimung@users.noreply.github.com>
2023-10-29 11:33:57 +0900

    test : Car 클래스 테스트 코드 추가 자동차 이름 유효성 검사, 자동차 이름 길이 검사, 자동차 전진 테스트, 자동차 출력 테스트 기능을 추가하였습니다

--- src/test/java/racingcar/CarTest.java
+++ src/test/java/racingcar/CarTest.java
@@ -1,16 +1,43 @@
 package racingcar;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 import racingcar.Car.Car;
 
 public class CarTest {
 
+
     @Test
-    void 자동차_전진_테스트() {
+    void 자동차_생성_테스트() {
+        Car car = new Car("povi");
+        assertThat("povi").isEqualTo(car.getCarName());
+    }
+
+    @Test
+    void 자동차_이름_유효성검사() {
+        assertThatThrownBy(() -> new Car(""))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차의 이름이 입력되지않았습니다.");
+    }
+
+    @Test
+    void 자동차_이름_길이검사() {
+        assertThatThrownBy(() -> new Car("123456"))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("자동차의 이름은 5이하만 가능합니다.");
+    }
+
+    @Test
+    void 자동차_전진_테스트(){
+        Car car = new Car("povi",1);
+        assertThat(car.getPosition()).isEqualTo(1);
+    }
+
+    @Test
+    void 자동차_출력_테스트() {
         Car car = new Car("povi", 2);
-        String input = car.printCurrentPosition();
-        assertThat(input).isEqualTo("povi : --");
+        assertThat(car.printCurrentPosition()).isEqualTo("povi : --");
     }
 }
