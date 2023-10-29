6331ebaebbbe53c40b88ea45ccc36b842d3b6e32
choimung <103022503+choimung@users.noreply.github.com>
2023-10-29 12:46:59 +0900

    test : CarList 클래스 테스트 코드  추가 자동차의 이름이 중복 검사, 모든 자동차 전진테스트, 한 명이 우승했을경우, 여러명이 우승했을경우 우승자를 출력해주는 테스트코드작성

--- src/test/java/racingcar/CarListTest.java
+++ src/test/java/racingcar/CarListTest.java
@@ -1,36 +1,56 @@
 package racingcar;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import racingcar.Car.Car;
 import racingcar.RacingCarGame.RacingCarGame;
 
 public class CarListTest {
     RacingCarGame game = new RacingCarGame();
 
     @Test
-    void 경주할_자동차_이름을_입력받는다() {
-        game.generateCars("pobi,woni,jun");
-    }
-
-    @Test
-    void 자동차의_이름이_없을경우_예외발생() {
-        assertThatThrownBy(() -> game.generateCars(" "))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차의 이름이 입력되지않았습니다.");
-    }
-
-    @Test
-    void 자동차의_이름이_5글자를넘을경우_예외발생() {
-        assertThatThrownBy(() -> game.generateCars("123456"))
-                .isInstanceOf(IllegalArgumentException.class)
-                .hasMessageContaining("자동차의 이름은 5글자를 넘을 수 없습니다.");
-    }
-
-    @Test
     void 자동차의_이름이_중복될경우_예외발생(){
         assertThatThrownBy(() -> game.generateCars("pobi,pobi"))
                 .isInstanceOf(IllegalArgumentException.class)
                 .hasMessageContaining("자동차의 이름이 중복됩니다.");
+    }
+
+
+    @Test
+    void 모든_자동차_전진_테스트(){
+        CarList carlist = new CarList(Arrays.asList(new Car("povi",2)
+                ,new Car("woni",3)
+                ,new Car("minu",5)));
+
+        List<String> carsPosition = carlist.AllCarCurrentPosition();
+        assertThat(carsPosition.get(0)).isEqualTo("povi : --");
+        assertThat(carsPosition.get(1)).isEqualTo("woni : ---");
+        assertThat(carsPosition.get(2)).isEqualTo("minu : -----");
+
+    }
+
+    @Test
+    void 한명이_우승했을때_출력(){
+        CarList carlist = new CarList(Arrays.asList(new Car("povi",2)
+        ,new Car("woni",3)
+        ,new Car("minu",5)));
+
+        String winner = carlist.WinnerList();
+        assertThat(winner).isEqualTo("minu");
+    }
+
+    @Test
+    void 여러명이_우승했을때_출력() {
+        CarList carlist = new CarList(Arrays.asList(new Car("povi", 5)
+                , new Car("woni", 5)
+                , new Car("minu", 5)));
+
+        String winner = carlist.WinnerList();
+
+        assertThat(winner).isEqualTo("povi, woni, minu");
     }
 }
